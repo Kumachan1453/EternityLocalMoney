dc897e3fb456ea915e5781afb251b9829e209a06
阪本周平 <github000only@gmail.com>
2023-10-10 20:37:58 +0900

    Homeスクリーン作成

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+// import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default Home;
